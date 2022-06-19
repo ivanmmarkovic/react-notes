f983981a4b6b15ce7a5551e95543a68f9d97e77d
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2022-06-19 18:06:50 +0200

    hooks/05-useLayoutEffect/src/components/MyComponent.js

--- hooks/05-useLayoutEffect/src/components/MyComponent.js
+++ hooks/05-useLayoutEffect/src/components/MyComponent.js
@@ -3,13 +3,6 @@ import React, {useEffect, useLayoutEffect, useRef} from "react";
 const MyComponent = () => {
 
     const inputRef = useRef();
-    /*
-
-    useLayoutEffect is called before the stuff is printed to the user
-
-    useEffect is called after everything is rendered
-
-    */
     
     useLayoutEffect(() => {
         console.log(inputRef.current.value);
