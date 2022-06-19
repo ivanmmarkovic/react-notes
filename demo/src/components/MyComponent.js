14a4d5adcfa4c1727f3f78befa7d2b34b87fbad0
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2022-06-19 17:52:15 +0200

    useMemo finished in demo project

--- demo/src/components/MyComponent.js
+++ demo/src/components/MyComponent.js
@@ -1,20 +1,45 @@
-import React, {useRef} from "react";
-import Button from "./Button";
+import React, {useRef, useState, useEffect, useMemo} from "react";
+import axios from 'axios';
+
 
 const MyComponent = () => {
 
-    const btnRef = useRef(null);
-    
-    const handleClick = () => {
-        btnRef.current.changeVisibility();
-    };
+    const [data, setData] = useState(null);
+    const [toggle, setToggle] = useState(false);
 
-    return (<div>
+    useEffect(() => {
+        axios
+            .get('https://jsonplaceholder.typicode.com/comments')
+            .then(response => {
+                setData(response.data);
+            });
+    }, []);
 
-        <button onClick={handleClick}>Parent button</button>
+    const findLongestName = (comments) => {
+        if(!comments) return null;
 
-        <Button ref={btnRef} />
-    </div>);
+        let longestName = '';
+        for(let i = 0; i < comments.length; i++){
+            let currentName = comments[i].name;
+            if(currentName.length > longestName.length){
+                longestName = currentName;
+            }
+        }
+        console.log('THIS WAS COMPUTED');
+        return longestName;
+    }
+
+    const getLongestName = useMemo(() => findLongestName(data), [data]);
+
+    return <div>
+
+        {/* <p>{findLongestName(data)}</p> */}
+        <p>{getLongestName}</p>
+        <button onClick={() => setToggle(!toggle)}>Toggle</button>
+        <br />
+        { toggle && <h1>toggle</h1> }
+
+    </div>
 };
 
 export default MyComponent;
