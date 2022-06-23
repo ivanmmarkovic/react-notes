5ed4d82eed7abd950facb723f103b94bb1fb492b
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2022-06-24 01:17:12 +0200

    useContext in demo project

--- demo/src/components/MyComponent.js
+++ demo/src/components/MyComponent.js
@@ -1,20 +1,13 @@
-import { useState, useCallback } from "react";
-import Child from './Child';
+import { useState, useCallback, useContext } from "react";
+import { LoginContext } from "../context/LoginContext";
 
 const MyComponent = () => {
     
-    const [visible, setVisible] = useState(false);
-    const [data, setData] = useState('DATA');
-
-    const returnComment = useCallback(() => {
-        return data;
-    }, [data]);
+    const {username, isLoggedIn} = useContext(LoginContext);
 
     return <div>
         <h1>MyComponent</h1>
-        <Child returnComment={returnComment} />
-        <button onClick={() => setVisible(!visible)}>Click</button>
-        {visible && <p>This is a text</p>}
+        {isLoggedIn && <p>{username}</p>}
     </div>
     
 };
