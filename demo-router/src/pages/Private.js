65733a7716a9c5cee31d0e31ff5add13810f2db0
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2022-06-26 14:44:21 +0200

    useNavigate

--- demo-router/src/pages/Private.js
+++ demo-router/src/pages/Private.js
@@ -1,9 +1,18 @@
+import { useNavigate } from "react-router-dom";
 
 
 const Private = () => {
 
+    const navigator = useNavigate();
+
+    const handleClick = () => {
+        navigator("/about");
+    };
+
     return <div>
         <h1>Private page</h1>
+
+        <button onClick={handleClick}>Go to about page</button>
     </div>
 };
 
