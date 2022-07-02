1f8c5c7650f0e652ac3b6ba79ce279ed3166664b
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2022-07-02 18:12:18 +0200

    useMemo

--- demo/src/components/MyComponent.js
+++ demo/src/components/MyComponent.js
@@ -1,18 +1,45 @@
 
-import {useEffect, useLayoutEffect, useRef, useState} from 'react';
+import {useEffect} from 'react';
 import axios from 'axios';
-import Button from './Button';
+import { useState } from 'react';
+import { useMemo } from 'react';
 
 const MyComponent = () => {
 
-    const btnRef = useRef(null);
+    const [visible, setVisible] = useState(false);
+    const [data, setData] = useState(null);
 
-    const handleClick = () => { btnRef.current.toggleVisibility() };
+    const handleClick = () => { setVisible(!visible) };
+
+    useEffect(() => {
+        axios
+            .get('https://jsonplaceholder.typicode.com/comments')
+            .then(response => {
+                setData(response.data);
+            });
+    }, []);
+
+    const findLongestName = (comments) => {
+        if(!comments) return null;
+
+        let longestName = '';
+        for(let i = 0; i < comments.length; i++){
+            let currentName = comments[i].name;
+            if(currentName.length > longestName.length){
+                longestName = currentName;
+            }
+        }
+        console.log('THIS WAS COMPUTED');
+        return longestName;
+    }
+
+    const longestName = useMemo(() => findLongestName(data), [data]);
 
     return <div>
         <h1>MyComponent</h1>
+        <p>Longest name : { longestName }</p>
         <button onClick={handleClick}>Parent button</button>
-        <Button ref={btnRef} />
+        { visible && <p>This is a text</p> }
     </div>
 
 };
